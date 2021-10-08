229775694a2a737d53bea50ba3da773d214c4fe0
Ismail55555 <92130294+Ismail55555@users.noreply.github.com>
2021-10-07 20:57:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function changecolor() {
+    document.getElementById("h1").style.color="yellow";
+} 
+function spanelem() {
+    document.getElementById("audio").play();
+    window.alert("This is a SPAN element");
+    }
+    function divelem() {
+    window.alert("This is a DIV element");
+    document.getElementById("audio").play();
+    }
+    function stop() {
+    document.getElementById("audio").pause();
+    }
